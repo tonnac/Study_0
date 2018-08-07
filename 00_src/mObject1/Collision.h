e34d537dcf432d89436b2c11c530eb4126e8f2e4
tonnac <tonnac35@gmail.com>
2018-08-07 18:11:23 +0900

    cccc

--- 00_src/mObject1/Collision.h
+++ 00_src/mObject1/Collision.h
@@ -5,11 +5,10 @@ class Collision
 {
 public:
 	static bool			RectInRect(RECT rtDesk, RECT rtSrc);
-	static bool			RectInPt(RECT rtDesk, POINT pt);
+	static bool			RectInPt(RECT rt, POINT pt);
 	static bool			SphereInSphere(RECT rtDesk, RECT rtSrc);
 	static bool			SphereInPt(RECT rtDesk, RECT rtSrc);
 public:
 	Collision();
 	virtual ~Collision();
 };
-
